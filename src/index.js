6c95a13e7f2777392367a54bbebda12afdf943ec
Hasan Naseem <hn92@live.com>
2018-06-29 19:37:28 -0400

    used abbreviated syntax for simplicity

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom';
 import Map from './components/map';
 import Filters from './components/filters';
 
-const api_link = 'https://services.arcgis.com/S9th0jAJ7bqgIRjw/arcgis/rest/services/KSI/FeatureServer/0/query?where=1%3D1&outFields=YEAR,DATE,TIME,Hour,STREET1,STREET2,OFFSET,ROAD_CLASS,District,LATITUDE,LONGITUDE,LOCCOORD,ACCLOC,TRAFFCTL,VISIBILITY,LIGHT,RDSFCOND,ACCLASS,IMPACTYPE,INVTYPE,INVAGE,INJURY,FATAL_NO,INITDIR,VEHTYPE,MANOEUVER,DRIVACT,DRIVCOND,PEDTYPE,PEDACT,PEDCOND,CYCLISTYPE,CYCACT,CYCCOND,PEDESTRIAN,CYCLIST,SPEEDING,REDLIGHT,ALCOHOL,DISABILITY,Division,Ward_Name,Ward_ID,Hood_ID,Hood_Name,FID,ACCNUM,Index_,AG_DRIV&outSR=4326&f=json'
-
-
 const parentNode = document.querySelector('.container')
+const api_link = 'https://services.arcgis.com/S9th0jAJ7bqgIRjw/arcgis/rest/services/KSI/FeatureServer/0/query?where=1%3D1&outFields=YEAR,DATE,TIME,Hour,STREET1,STREET2,OFFSET,ROAD_CLASS,District,LATITUDE,LONGITUDE,LOCCOORD,ACCLOC,TRAFFCTL,VISIBILITY,LIGHT,RDSFCOND,ACCLASS,IMPACTYPE,INVTYPE,INVAGE,INJURY,FATAL_NO,INITDIR,VEHTYPE,MANOEUVER,DRIVACT,DRIVCOND,PEDTYPE,PEDACT,PEDCOND,CYCLISTYPE,CYCACT,CYCCOND,PEDESTRIAN,CYCLIST,SPEEDING,REDLIGHT,ALCOHOL,DISABILITY,Division,Ward_Name,Ward_ID,Hood_ID,Hood_Name,FID,ACCNUM,Index_,AG_DRIV&outSR=4326&f=json'
 
 class App extends Component {
   constructor(props) {
@@ -20,7 +18,7 @@ class App extends Component {
   componentDidMount() {
   fetch(api_link)
     .then(response => response.json())
-    .then(data => this.setState({ data: data }, function() {
+    .then(data => this.setState({ data }, function() {
       console.log(this.state.data.features);
     }));
   }
