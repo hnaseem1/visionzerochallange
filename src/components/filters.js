5b352afc76ceead96d95c6940f4e37d254263bc5
Hasan Naseem <hn92@live.com>
2018-06-28 20:57:47 -0400

    added class based componenets code

---
+++ src/components/filters.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class filters extends Component {
+  constructor() {
+    super(props)
+  }
+}
+
+export default map;
---
+++ src/components/map.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class map extends Component {
+  constructor() {
+    super(props)
+  }
+}
+
+export default map;
