c72f34b30cbc6f987599ec2ebdb3b16c648ae3b2
Hasan Naseem <hn92@live.com>
2018-06-29 19:21:36 -0400

    fetched api data and set state.data as json

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Map from './components/map';
 import Filters from './components/filters';
 
+const api_link = 'https://services.arcgis.com/S9th0jAJ7bqgIRjw/arcgis/rest/services/KSI/FeatureServer/0/query?where=1%3D1&outFields=YEAR,DATE,TIME,Hour,STREET1,STREET2,OFFSET,ROAD_CLASS,District,LATITUDE,LONGITUDE,LOCCOORD,ACCLOC,TRAFFCTL,VISIBILITY,LIGHT,RDSFCOND,ACCLASS,IMPACTYPE,INVTYPE,INVAGE,INJURY,FATAL_NO,INITDIR,VEHTYPE,MANOEUVER,DRIVACT,DRIVCOND,PEDTYPE,PEDACT,PEDCOND,CYCLISTYPE,CYCACT,CYCCOND,PEDESTRIAN,CYCLIST,SPEEDING,REDLIGHT,ALCOHOL,DISABILITY,Division,Ward_Name,Ward_ID,Hood_ID,Hood_Name,FID,ACCNUM,Index_,AG_DRIV&outSR=4326&f=json'
 
 
 const parentNode = document.querySelector('.container')
@@ -11,10 +12,17 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    // this.state = {
-    //   // for later
-    //   stateObject = null,
-    // }
+    this.state = {
+      data: null
+    }
+  }
+
+  componentDidMount() {
+  fetch(api_link)
+    .then(response => response.json())
+    .then(data => this.setState({ data: data }, function() {
+      console.log(this.state.data.features);
+    }));
   }
 
   render() {
@@ -31,6 +39,7 @@ class App extends Component {
       </div>
     );
   }
+
 }
 
 ReactDOM.render(<App/>, parentNode);
