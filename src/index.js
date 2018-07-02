ccfb4f0b3c3a38e1713f61788919b10556b55503
gusmattos1 <gusmattos1@gmail.com>
2018-07-02 18:05:35 -0400

    maps working with local data

--- src/index.js
+++ src/index.js
@@ -1,36 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Map from './components/map';
-import Filters from './components/filters';
+import App from './components/App';
 
 
 
 const parentNode = document.querySelector('.container')
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    // this.state = {
-    //   // for later
-    //   stateObject = null,
-    // }
-  }
-
-  render() {
-    return (
-      <div>
-        <Map
-          isMarkerShown
-          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `400px` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />
-        <Filters />
-      </div>
-    );
-  }
-}
 
 ReactDOM.render(<App/>, parentNode);
