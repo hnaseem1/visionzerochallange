dcbbf9f59187ca045b8eec112f9c3be9e8534bf1
Karen <kjgamis@live.ca>
2018-07-23 19:36:24 -0400

    add tab styling

--- index.js
+++ index.js
@@ -434,7 +434,7 @@ function openTab(evt, tabName) {
     }
     tablinks = document.querySelectorAll(".tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace("active", "");
     }
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
